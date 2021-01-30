8ddf678669346c538bb95afba3ae70e742a7c089
tam07 <vuminhtam.sdb@gmail.com>
2021-01-30 11:09:53 +0700

    d1

---
+++ Pronunciation_English_2/database/config.php
@@ -0,0 +1,7 @@
+<?php
+	define("sql_server","localhost");
+    define("sql_user","tamtam_2");
+    define("sql_password","tamtam");
+	define("sql_database","Pronunciation_English_2");
+	define("BASE_URL","http://localhost/Pronunciation_English_2/");
+?>
---
+++ Pronunciation_English_2/index.php
@@ -0,0 +1,9 @@
+ <script type="text/javascript">
+ 	
+
+ window.onload = function () {
+
+ 	window.location.replace("http://localhost/Pronunciation_English_2/view/main_0.html");
+}
+
+ </script>
